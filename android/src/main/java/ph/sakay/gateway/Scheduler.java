3a1cd74617990d8acfc55083befc0d4cb4635548
Thomas Dy <thatsmydoing@gmail.com>
2013-09-29 12:53:33 +0800

    [Gateway] Add logging for scheduler

--- android/src/main/java/ph/sakay/gateway/Scheduler.java
+++ android/src/main/java/ph/sakay/gateway/Scheduler.java
@@ -11,6 +11,7 @@ import android.util.Log;
 public class Scheduler extends BroadcastReceiver {
 
 	public void onReceive(Context context, Intent intent) {
+		Log.d("Scheduler", "Scheduling polling service every 1 minute");
 		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 		PendingIntent pi = PendingIntent.getService(context, 1, new Intent(context, PollingService.class), 0);
 		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+60000, 60000, pi);
