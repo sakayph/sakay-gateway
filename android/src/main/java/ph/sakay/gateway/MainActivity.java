1f50fc33cd22a3c33e42ff6679298427e75efc95
Thomas Dy <thatsmydoing@gmail.com>
2013-09-29 13:37:14 +0800

    [Gateway] Allow toggling polling

--- android/src/main/java/ph/sakay/gateway/MainActivity.java
+++ android/src/main/java/ph/sakay/gateway/MainActivity.java
@@ -1,6 +1,7 @@
 package ph.sakay.gateway;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 public class MainActivity extends PreferenceActivity {
@@ -8,5 +9,18 @@ public class MainActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.main);
+
+		findPreference("polling_enabled").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+			public boolean onPreferenceChange(Preference preference, Object value) {
+				boolean val = (Boolean) value;
+				if(val) {
+					Scheduler.schedule(MainActivity.this);
+				}
+				else {
+					Scheduler.cancel(MainActivity.this);
+				}
+				return true;
+			}
+		});
 	}
 }
--- android/src/main/java/ph/sakay/gateway/Scheduler.java
+++ android/src/main/java/ph/sakay/gateway/Scheduler.java
@@ -5,16 +5,30 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.os.SystemClock;
 import android.util.Log;
 
 public class Scheduler extends BroadcastReceiver {
 
 	public void onReceive(Context context, Intent intent) {
+		if(PreferenceManager.getDefaultSharedPreferences(context).getBoolean("polling_enabled", true)) {
+			schedule(context);
+		}
+	}
+
+	public static void schedule(Context context) {
 		Log.d("Scheduler", "Scheduling polling service every 1 minute");
 		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 		PendingIntent pi = PendingIntent.getService(context, 1, new Intent(context, PollingService.class), 0);
 		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+60000, 60000, pi);
+	}
+
+	public static void cancel(Context context) {
+		Log.d("Scheduler", "Disabling polling");
+		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		PendingIntent pi = PendingIntent.getService(context, 1, new Intent(context, PollingService.class), 0);
+		am.cancel(pi);
 	}
 
 }
