7d828c22954ace5b3228598c9f2b120edb7def96
Thomas Dy <thatsmydoing@gmail.com>
2013-09-29 13:51:56 +0800

    Add PostgreSQL drivers

--- project/Build.scala
+++ project/Build.scala
@@ -10,12 +10,13 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "postgresql" % "postgresql" % "9.1-901.jdbc4"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
   )
 
 }
