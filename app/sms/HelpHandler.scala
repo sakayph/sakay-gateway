b7c258091772977a6912d2d3f9072adaeebe02b9
Thomas Dy <thatsmydoing@gmail.com>
2013-09-30 18:17:20 +0800

    Update help

--- app/sms/HelpHandler.scala
+++ app/sms/HelpHandler.scala
@@ -4,6 +4,6 @@ object HelpHandler extends StaticHandler {
   def keyword = "HELP"
 
   val reply = "To get directions, text ROUTE <from> TO <to>, example: "+
-              "ROUTE ateneo TO UP.\n"+
+              "ROUTE ateneo TO up diliman.\n"+
               "To get jeeps passing near you, text NEAR <location>"
 }
